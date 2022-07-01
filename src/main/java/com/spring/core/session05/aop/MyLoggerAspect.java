d59e8fe0de4e9d5ffd50a7926910a0b2ff582073
user <vichsu38@gmail.com>
2022-07-01 21:44:10 +0800

    0701

--- src/main/java/com/spring/core/session05/aop/MyLoggerAspect.java
+++ src/main/java/com/spring/core/session05/aop/MyLoggerAspect.java
@@ -1,6 +1,8 @@
 package com.spring.core.session05.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,15 @@ import org.springframework.stereotype.Component;
            // 使用時機 : 有多個切面程式 , 在同一個橫切關注點(方法)要執行時
 public class MyLoggerAspect {
 
-	
-	
+  	// 前置(Before) 通知(Advice)
+	// joinpoint 連結點
+	// 切入點表達式 Spring EL : execution(..)
+	@Before(value = "execution(public Integer com.spring.core.session05.aop.MathCalcImpl.add(Integer,Integer); )") 
+	public void beforeAdvice(JoinPoint joinPoint) {
+		// 取得連接點方法(方法簽章)名稱
+		String methodName = joinPoint.getSignature().getName();
+		// 方法參數
+	    Object[] args = joinPoint.getArgs();
+	    System.out.printf("前置通知 : 方法名稱 %s 參數%s \n" , methodName , args);
+	}
 }
