df7f75458a877a15089900dc4820a702e8a9ff53
user <vichsu38@gmail.com>
2022-07-01 19:14:58 +0800

    0701

---
+++ src/main/java/com/spring/core/session04/dyn/CalcImpl.java
@@ -0,0 +1,17 @@
+package com.spring.core.session04.dyn;
+
+public class CalcImpl implements Calc {
+
+	@Override
+	public int add(int x, int y) {
+		int result = x + y;
+		return result;
+	}
+
+	@Override
+	public int div(int x, int y) {
+		int result = x * y;
+		return result;
+	}
+
+}
