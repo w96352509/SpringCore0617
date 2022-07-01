c73fea38cfad32e414c6b8f48df3aff3489e69b5
user <vichsu38@gmail.com>
2022-07-01 21:01:48 +0800

    0701

--- src/test/java/com/spring/core/session04/CglibTest.java
+++ src/test/java/com/spring/core/session04/CglibTest.java
@@ -1,11 +1,23 @@
 package com.spring.core.session04;
 
+import org.springframework.cglib.proxy.Enhancer;
+
+import com.spring.core.session04.cglib.*;
+import com.spring.core.session04.cglib.MyMethodInterceptor;
+
 public class CglibTest {
 
 	public void test() {
 		// 使用 cglib
 		// 1. 增強器
-		
+		Enhancer enhancer = new Enhancer();
+		// 2. 要增強的對象
+		enhancer.setSuperclass(Hello.class);
+		// 3. 方法攔截器
+		enhancer.setCallback(new MyMethodInterceptor());
+		// 4.
+		Hello hello = (Hello)enhancer.create();
+		System.out.println(hello.sayHello("John"));
 	}
 	
 }
