86313be4c57c61af3b532358fa93cd5bbc350c27
user <vichsu38@gmail.com>
2022-07-18 18:40:48 +0800

    0718

--- src/test/java/com/spring/core/session07/BookTest.java
+++ src/test/java/com/spring/core/session07/BookTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.spring.core.session07.tx.controller.BookController;
 import com.spring.core.session07.tx.dao.BookDao;
 import com.spring.core.session07.tx.exception.InsufficientAmount;
 import com.spring.core.session07.tx.exception.InsufficientQuantity;
@@ -13,14 +14,7 @@ public class BookTest {
 	@Test
 	public void test() throws InsufficientQuantity , InsufficientAmount {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
-		BookDao bookDao = ctx.getBean(BookDao.class);
-		// 書號 要減去數量
-		bookDao.updateStock(1,20);
-		// 錢包號碼 要減去金額
-		bookDao.updateWallet(1, 400);
-		System.out.println("書本金額" + bookDao.getPrice(1));
-		System.out.println("庫存數量" + bookDao.getStockAmount(1));
-		System.out.println("客戶金額" + bookDao.getWallentMoney(1));
+		BookController bookController = ctx.getBean(BookController.class);
 		
 	}
 	
